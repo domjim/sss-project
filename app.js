5cf7a8ba629c52117ae5e86d5668000bde3e8122
domjim <43128112+domjim@users.noreply.github.com>
2018-09-11 11:30:22 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/1", function(req,res){
   res.redirect("/project_1/index.html");
 });
 
+app.get("/2", function(req,res){
+  res.redirect("/project_2/index.html");
+});
+
 // Port setting
 app.listen(process.env.PORT || 3000, function(){
   console.log("server on!");
